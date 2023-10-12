d90222166a676df445419eeee2e1e8d6e262362c
shital <145319502+Shital970@users.noreply.github.com>
2023-10-12 12:58:57 +0530

    first commit

---
+++ Dayone/src/com/tnsif/dayone/IfelseDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.dayone;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+		
+		int age=1;
+		
+		if(age>=18)
+			System.out.println("You are eligible for Voting");
+		else
+			
+			System.out.println("Sorry!You need to wait moe");
+		
+		System.out.println("if else demonstration");
+
+	}
+
+}
